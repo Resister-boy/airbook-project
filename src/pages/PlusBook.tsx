58827e1a7fa8b6e36144a03f9bdbc68598e80aaf
Resister-boy <hassanpumped17@gmail.com>
2022-11-14 00:32:13 +0900

    Project Router Settings & Error Boundary Settings

---
+++ src/pages/PlusBook.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PlusBook = () => {
+  return (
+    <div>PlusBook</div>
+  )
+}
+
+export default PlusBook
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Home from "./Home";
+import EditBook from "./EditBook";
+import ShowBook from "./ShowBook";
+import PlusBook from "./PlusBook";
+import NotFound from "./NotFound";
+import SignIn from "./SignIn";
+import Error from "./Error";
+
+export {
+  Home,
+  EditBook,
+  ShowBook,
+  PlusBook,
+  NotFound,
+  SignIn,
+  Error
+}
